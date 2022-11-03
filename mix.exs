98972faa29870e3d1d298bfe05e8737f412b9d06
Jhonatan Martins <jhonatanbmartins@gmail.com>
2022-11-03 11:29:15 -0300

    chore: up to 1.0 version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExPetOauth.MixProject do
   def project do
     [
       app: :ex_pet_oauth,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
