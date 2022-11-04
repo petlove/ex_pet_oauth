114e689585a1fc18a5f7ba1b7a65116cf1baaa97
Jhonatan Martins <jhonatanbmartins@gmail.com>
2022-11-04 17:17:24 -0300

    fix: warning in application

--- lib/ex_pet_oauth/application.ex
+++ lib/ex_pet_oauth/application.ex
@@ -22,14 +22,5 @@ defmodule ExPetOauth.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ExPetOauth.Supervisor]
     Supervisor.start_link(children, opts)
-    # end
-  end
-
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  @impl true
-  def config_change(changed, _new, removed) do
-    ExPetOauthDummyWeb.Endpoint.config_change(changed, removed)
-    :ok
   end
 end
